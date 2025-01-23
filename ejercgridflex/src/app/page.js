33cf097b5e241749d2f101e3212247158c10a8e7
T3d4xjr <tedaxjr@gmail.com>
2025-01-23 22:05:41 +0100

    UPD:Estilos creados

--- ejercgridflex/src/app/page.js
+++ ejercgridflex/src/app/page.js
@@ -1,35 +1,39 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import "@/app/styles/global.css";
 export default function Home() {
   return (
     <div>
-            <header>
-                <img className="logo" src="logoEldenRing.png"></img>
-                <nav className="menu">
-                    <ul>
-                        <li><a href="#">Inicio</a></li>
-                        <li><a href="#">Servicios</a></li>
-                        <li><a href="#">Contacto</a></li>
-                    </ul>
-                </nav>
-            </header>
+      <header>
+        <img className="logo" src="logoEldenRing.png"></img>
+        <nav className="menu">
+          <ul>
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">Servicios</a>
+            </li>
+            <li>
+              <a href="#">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-            {/* Sección principal */}
-            <main>
-                <section className="grid-container">
-                    <div className="grid-item item1">Bloque 1</div>
-                    <div className="grid-item item2">Bloque 2</div>
-                    <div className="grid-item item3">Bloque 3</div>
-                    <div className="grid-item item4">Bloque 4</div>
-                    <div className="grid-item item5">Bloque 5</div>
-                </section>
-            </main>
+      <main>
+        <section className="grid-container">
+          <img className="griditem1" src="maliketh.jpg"></img>
+          <img className="griditem2" src="godrick.jpg"></img>
+          <img className="griditem3" src="malenia.jpg"></img>
+          <img className="griditem4" src="radahn.jpg"></img>
+          <img className="griditem5" src="radagon.jpg"></img>
+        </section>
+      </main>
 
-            {/* Pie de página */}
-            <footer>
-                <p>© 2025 Mi Sitio Web</p>
-            </footer>
-        </div>
+      <footer>
+        <p>© 2025 Mi Sitio Web</p>
+      </footer>
+    </div>
   );
 }
